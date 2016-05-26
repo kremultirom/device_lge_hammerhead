1e5d727e37b153e77868df4dffa8db6aa88d58cf
George G <kreach3r@users.noreply.github.com>
2016-05-26 17:39:33 +0300

    Update encryption to Krexus vendor/lge paths

--- multirom/mr_cp_crypto.sh
+++ multirom/mr_cp_crypto.sh
@@ -5,9 +5,9 @@
 QCOM_KEYMASTER_FILES="keymaster.b00 keymaster.b01 keymaster.b02 keymaster.b03 keymaster.mdt"
 QCOM_CMNLIB_FILES="cmnlib.b00 cmnlib.b01 cmnlib.b02 cmnlib.b03 cmnlib.mdt"
 QCOM_LIB_FILES="libQSEEComAPI.so"
-QCOM_PATH="$1/vendor/qcom/hammerhead/proprietary/"
+QCOM_PATH="$1/vendor/lge/hammerhead/proprietary/"
 
-cp -a "${QCOM_PATH}/libQSEEComAPI.so" "$2/"
+cp -a "${QCOM_PATH}/lib/libQSEEComAPI.so" "$2/"
 
 # libQSEEComApi depends on libutils. These are built from omni source and libutils
 # is modified to drop libbacktrace dependency
@@ -19,10 +19,9 @@ mkdir -p "$2/vendor/lib/hw/"
 cp -a "$1/device/lge/hammerhead/recovery/keystore.msm8974.so" "$2/vendor/lib/hw/keystore.default.so"
 
 for f in $QCOM_CMNLIB_FILES; do
-    cp -a "${QCOM_PATH}/${f}" "$2/vendor/firmware/"
+    cp -a "${QCOM_PATH}/vendor/firmware/${f}" "$2/vendor/firmware/"
 done
 
-# Keymaster files are not in hammerhead's vendor/ files, because fuck you.
 for f in $QCOM_KEYMASTER_FILES; do
-    cp -a "$1/device/lge/hammerhead/recovery/${f}" "$2/vendor/firmware/keymaster/"
+    cp -a "${QCOM_PATH}/vendor/firmware/keymaster/${f}" "$2/vendor/firmware/keymaster/"
 done
