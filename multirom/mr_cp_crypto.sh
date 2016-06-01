6cbe752fee8fd99e62191f1ab02a2f8227bede45
George G <kreach3r@users.noreply.github.com>
2016-06-02 01:23:07 +0300

    Mrom crypto: update paths to DonkeyCoyote blobs

--- multirom/mr_cp_crypto.sh
+++ multirom/mr_cp_crypto.sh
@@ -5,7 +5,7 @@
 QCOM_KEYMASTER_FILES="keymaster.b00 keymaster.b01 keymaster.b02 keymaster.b03 keymaster.mdt"
 QCOM_CMNLIB_FILES="cmnlib.b00 cmnlib.b01 cmnlib.b02 cmnlib.b03 cmnlib.mdt"
 QCOM_LIB_FILES="libQSEEComAPI.so"
-QCOM_PATH="$1/vendor/lge/hammerhead/proprietary/"
+QCOM_PATH="$1/vendor/lge/hammerhead/qcom/hammerhead/proprietary/"
 
 cp -a "${QCOM_PATH}/lib/libQSEEComAPI.so" "$2/"
 
@@ -16,12 +16,12 @@ cp -an "$1/device/lge/hammerhead/multirom/libstlport.so" "$2/"
 
 mkdir -p "$2/vendor/firmware/keymaster"
 mkdir -p "$2/vendor/lib/hw/"
-cp -a "$1/device/lge/hammerhead/recovery/keystore.msm8974.so" "$2/vendor/lib/hw/keystore.default.so"
+cp -a "$1/vendor/lge/hammerhead/qcom/hammerhead/proprietary/lib/hw/keystore.msm8974.so" "$2/vendor/lib/hw/keystore.default.so"
 
 for f in $QCOM_CMNLIB_FILES; do
-    cp -a "${QCOM_PATH}/vendor/firmware/${f}" "$2/vendor/firmware/"
+    cp -a "${QCOM_PATH}/${f}" "$2/vendor/firmware/"
 done
 
 for f in $QCOM_KEYMASTER_FILES; do
-    cp -a "${QCOM_PATH}/vendor/firmware/keymaster/${f}" "$2/vendor/firmware/keymaster/"
+    cp -a "$1/vendor/lge/hammerhead/lge/hammerhead/proprietary/${f}" "$2/vendor/firmware/keymaster/"
 done
